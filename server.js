4a543832f8909484c304383ad347096774ddb8ed
Vincent Fuertez <vfuertez@gmail.com>
2022-11-30 19:54:46 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 4321
+
+
+app.get('/', (request, response) => {
+    response.send(`hello world i am listening to port ${PORT}`)
+  })
+
+  app.get('/dburl', (request, response) => {
+    response.send(`My connection string is: ${process.env.DATABASE_URL}`)
+  });
+
+
+
+
+  
+app.listen(PORT, () => {
+    console.log(`✅ PORT: ${PORT} 🌟`); 
+  });
